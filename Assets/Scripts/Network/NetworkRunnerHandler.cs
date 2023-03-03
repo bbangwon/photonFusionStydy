384788e9f571a515db40a49c1162e22d1e32eec5
bbangwon <bbangwon.chung@gmail.com>
2023-03-03 10:21:37 +0900

    네트워크 러너 추가

--- Assets/Scripts/Network/NetworkRunnerHandler.cs
+++ Assets/Scripts/Network/NetworkRunnerHandler.cs
@@ -1,18 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
+using Fusion;
+using Fusion.Sockets;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NetworkRunnerHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public NetworkRunner networkRunnerPrefab;
+
+    NetworkRunner networkRunner;
+
     void Start()
     {
-        
+        networkRunner = Instantiate(networkRunnerPrefab);
+        networkRunner.name = "Network runner";
+
+        var clientTask = InitializeNetworkRunner(networkRunner, GameMode.AutoHostOrClient, NetAddress.Any(), SceneManager.GetActiveScene().buildIndex, null);
+
+        Debug.Log($"Server NetworkRunner started.");
     }
 
-    // Update is called once per frame
-    void Update()
+    protected virtual Task InitializeNetworkRunner(NetworkRunner runner, GameMode gameMode, NetAddress address, SceneRef scene, Action<NetworkRunner> initialized)
     {
-        
+        var sceneManager = runner.GetComponents(typeof(MonoBehaviour)).OfType<INetworkSceneManager>().FirstOrDefault();
+
+        if(sceneManager != null)
+        {
+            sceneManager = runner.gameObject.AddComponent<NetworkSceneManagerDefault>();
+        }
+
+        runner.ProvideInput = true;
+
+        return runner.StartGame(new StartGameArgs
+        {
+            GameMode = gameMode,
+            Address = address,
+            Scene = scene,
+            SessionName = "TestRoom",
+            Initialized = initialized,
+            SceneManager = sceneManager
+        });
+
     }
 }
