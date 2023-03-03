ca9e80e39eb1a5a3cea13f9dc3925b7bafd4758e
bbangwon <bbangwon.chung@gmail.com>
2023-03-03 11:50:58 +0900

    플레이어 스폰

---
+++ Assets/Scripts/Utils/Utils.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public static class Utils
+{
+    public static Vector3 GetRandomSpawnPoint()
+    {
+        return new Vector3(Random.Range(-20, 20), 4, Random.Range(-20, 20));
+    }
+}
